8a437037c4f9d119f88725be321fdc4d903edd49
snownisha <snownisha123@gmail.com>
2024-05-10 20:04:30 +0200

    Created database for task

--- tmp/tasks/models.py
+++ tmp/tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    priority=models.CharField(max_length=10, choices=[('High', 'High'), ('Medium', 'Medium'), ('Low', 'Low')])
+    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('In Progress', 'In Progress'), ('Completed', 'Completed')])
