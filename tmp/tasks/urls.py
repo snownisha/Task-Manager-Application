25e8f217c68fb758236ab403506132f5f5dcd96e
snownisha <snownisha123@gmail.com>
2024-05-11 06:34:32 +0200

    Created & updated endpoints

--- tmp/tasks/urls.py
+++ tmp/tasks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tasks/', views.get_tasks),
-    path('tasks/add/', views.add_tasks),
-    path('tasks/<int:task_id>/', views.update_task),
+    path('tasks/', views.get_tasks, name="get tasks"),
+    path('tasks/add/', views.add_tasks, name="view tasks"),
+    path('tasks/<int:task_id>/', views.update_task, name="update tasks"),
     path('tasks/filter/<str:priority>/', views.filter_tasks_by_priority),
 ]
